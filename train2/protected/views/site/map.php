95542156440b626cd58784b99ad43eca016d018d
thanawatKow <thanawat_ben@hotmail.com>
2014-11-12 20:14:08 +0700

    Revert "-"
    This reverts commit 000e64e89e12f8dfbebe2b65cccb6ab0739926c8.

--- train2/protected/views/site/map.php
+++
@@ -1,6 +0,0 @@
-    <h1>Welcome to <i></i></h1>
-
-                <p></p>
-                <p></p>
-                <p> </p>
-                <img width="670" src="<?php echo Yii::app()->request->baseUrl; ?>/images/Map.jpg" alt="Avatar">
